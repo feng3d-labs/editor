f5bd04ba7d44bd2a52319120e2a3dc9e2762b0f7
feng <908087098@qq.com>
2022-07-29 14:48:24 +0800

    使用electron运行

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-"use strict";
+const { app, BrowserWindow } = require('electron');
 
-const { app, BrowserWindow } = require('electron')
-
-require("./native/electron_main.js")
+require('./native/electron_main.js');
 
 app.commandLine.appendSwitch('enable-unsafe-es3-apis');
-app.on("ready", function ()
+app.on('ready', function ()
 {
-    //ico--package
-    //webPreferences:Settings of web page’s features
-    var mainWindow = new BrowserWindow({
+    // ico--package
+    // webPreferences:Settings of web page’s features
+    const mainWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true,
             webSecurity: true
         },
         width: 800, height: 600, minWidth: 800, minHeight: 600
     });
-    mainWindow.loadURL("file://" + __dirname + "/index.html");
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+
     // mainWindow.webContents.openDevTools();
+
     // console.log('app.ready:...chrome=', process.versions['chrome'], process.versions['v8']);
 });
